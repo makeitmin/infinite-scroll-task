39927ed49350e4ba7fe60e9d042c27c561f80aea
makeitmin <seongminha0910@gmail.com>
2021-06-28 10:25:19 +0000

    Feat: Type A 게시물 API 호출

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL, API_KEY } from './config.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [typeAPosts, setTypeAPosts] = useState();
+
+  useEffect(() => {
+    if (!typeAPosts) {
+      axios
+        .get(`${API_URL}/${API_KEY}/a-posts?page=0`)
+        .then(function (response) {
+          console.log('포스트 호출 성공');
+          console.log(response.data);
+          setTypeAPosts(response.data);
+        })
+        .catch(function (error) {
+          console.log('포스트 호출 실패');
+          console.log(error);
+        });
+    }
+  });
+
+  let typeALists = '';
+  if (!!typeAPosts) {
+    typeALists = typeAPosts.map((post) => <li>{post.title}</li>);
+  }
+
+  return <div>{typeALists}</div>;
 }
 
 export default App;
